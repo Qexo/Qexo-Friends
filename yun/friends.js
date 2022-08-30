a4af4773d6ab87e41579e9ad476f686ca41ad870
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-08-30 23:06:50 +0800

    Update friends.js

--- yun/friends.js
+++ yun/friends.js
@@ -1,32 +1,12 @@
-function loadQexoFriends(id, url, color) {
+function loadQexoFriends(id, url) {
     var uri = url + "/pub/friends/";
-    var loadStyle = '<div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div>';
-    document.getElementById(id).className = "Qexo-friends";
+    var loadStyle = '<center><div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div><center>';
+    document.getElementById(id).className = "friend-content";
     document.getElementById(id).innerHTML = loadStyle;
-    var ajax;
-    try {
-        // Firefox, Opera 8.0+, Safari
-        ajax = new XMLHttpRequest();
-    } catch (e) {
-        // Internet Explorer
-        try {
-            ajax = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                ajax = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {
-                alert("糟糕,你的浏览器不能上传文件!");
-                return false;
-            }
-        }
-    }
-    ajax.open("get", uri, true);
-    ajax.setRequestHeader("Content-Type", "text/plain");
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState == 4) {
-            if (ajax.status == 200) {
-                var res = JSON.parse(ajax.response);
-                if (res["status"]) {
+    fetch(uri).then(function(data){
+        if (data.ok){
+            data.json().then(function(res){
+            if (res["status"]) {
                     var friends = res["data"];
                     document.getElementById(id).innerHTML = '<ul class="link-items">';
                     for (let i = 0; i < friends.length; i++) {
@@ -36,10 +16,9 @@ function loadQexoFriends(id, url, color) {
                 } else {
                     alert("友链载入失败!");
                 }
-            } else {
-                alert("友链获取失败!");
-            }
+            });
+        } else {
+            alert("友链获取失败!");
         }
-    };
-    ajax.send(null);
+    });
 }
