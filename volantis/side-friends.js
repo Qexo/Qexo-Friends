09e6b43fe6364e5ba6896eaddd8b17287de440bb
crsn <92655031+Fgaoxing@users.noreply.github.com>
2022-12-30 23:52:50 +0800

    优化代码

--- volantis/side-friends.js
+++ volantis/side-friends.js
@@ -8,9 +8,37 @@ function loadQexoFriends(id, url) {
             data.json().then(function(res){
             if (res["status"]) {
                     var friends = res["data"];
-                    document.getElementById(id).innerHTML = "";
+                    document.getElementById(id).innerHTML = `
+ <style>
+.Qfriends_line {
+        background: #fff;
+        border-radius: 5px !important;
+        margin: 3px 0 !important;
+}
+.Qfriends_line:hover {
+        background: #a4a4a485 !important;
+}
+.Qfriends_line > a > .name {
+    display: flex !important;
+    flex-direction: row !important;
+    align-items: center !important;
+    padding: 3px !important;
+}
+.Qfriends_line > a > .name > p {
+        margin: 0 5px !important;
+        font-weight: bold;color: #000 !important;
+}
+</style>`;
                     for (let i = 0; i < friends.length; i++) {
-                        document.getElementById(id).innerHTML += '<li><a class="flat-box" title="' + friends[i]["description"] + '" target="_blank" rel="noopener" href="' + friends[i]["url"] + '" active-action="action-' + friends[i]["url"] + '"><div class="name"><img class="lazyload loaded" alt="哎呀，图片丢了" src="' + friends[i]["image"] + '" srcset="' + friends[i]["image"] + '" data-ll-status="loaded" height="20" width="20">' + friends[i]["name"] + '</div></a></li>'; 
+                        document.getElementById(id).innerHTML += `
+                        <li class="Qfriends_line">
+                        <a class="flat-box" title="${friends[i]["description"]}" target="_blank" rel="noopener" href="${friends[i]["url"]}">
+                        <div class="name">
+                        <img src="${friends[i]["image"]}"  height="20" width="20" style="border-radius: 100%;">
+                        ${friends[i]["name"]}
+                        </div>
+                        </a>
+                        </li>`; 
                     }
                     document.getElementById(id).innerHTML += '';
                 } else {
